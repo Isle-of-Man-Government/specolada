60be7083a10cf64763523786077d20616dc293fb
Fortin <GTSGFOR@gov.im>
2020-05-13 14:45:47 +0100

    Clean 'Page' model from 'TreeNode' pollution

--- src/Model/Page.ts
+++ src/Model/Page.ts
@@ -1,15 +1,14 @@
 import { FieldGroup } from "./FieldGroup";
 import { Field } from "./Field";
-import { TreeNode } from 'Store';
 
 
 export class Page {
     title: string;
-    content: (TreeNode & (FieldGroup | Field))[];
+    content: (FieldGroup | Field)[];
 
     constructor(
         title: string,
-        content: (TreeNode & (FieldGroup | Field))[] = [],
+        content:  (FieldGroup | Field)[] = [],
     ) {
         this.title = title;
         this.content = content;
