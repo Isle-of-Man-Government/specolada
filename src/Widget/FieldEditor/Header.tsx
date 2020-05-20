88b25172aeafd1b7a3ae535bf14a816d2531bc3e
Fortin <GTSGFOR@gov.im>
2020-05-20 12:30:52 +0100

    Refactor Header in FieldEditor

--- src/Widget/FieldEditor/Header.tsx
+++ src/Widget/FieldEditor/Header.tsx
@@ -12,33 +12,55 @@ interface HeaderProps {
     onSaveClick: () => void;
 }
 
-export const Header: React.FC<HeaderProps> =
-    ({ field, showPropsPreview, showSaveControls, onHeaderClick, onSaveClick }) =>
-        <Flex onClick={onHeaderClick} margin={1}>
-            <Text as="span">
-                {field?.title}
-            &nbsp; {/* effectively, this serves as 'min height' */}
-            </Text>
-            <Collapse isOpen={showPropsPreview}
-                marginLeft={4}
-            >
-                <Text
-                    as="span"
-                    fontSize={14}
-                    color="rgba(150, 150, 150, 0.3)"
-                >
-                    other props will show here
-            </Text>
-            </Collapse>
-            <Button
-                variantColor="green"
-                size="sm"
-                visibility={showSaveControls ? "visible" : "hidden"}
-                marginLeft="auto"  // to keep it on the right if it spills to the next line
-                border={0}
-                opacity={0.8}
-                onClick={onSaveClick}
-            >
-                Save
-        </Button>
+export const Header: React.FC<HeaderProps> = (
+    { field, showPropsPreview, showSaveControls, onHeaderClick, onSaveClick }
+) =>
+    <Flex
+        onClick={onHeaderClick}
+            margin={1}
+            flexWrap="wrap"
+        >
+            <TitleOrReplacementText title={field.title} />
+            <PropsPreview show={showPropsPreview} />
+            <SaveButton show={showSaveControls} onSaveClick={onSaveClick} />
         </Flex>;
+
+const TitleOrReplacementText: React.FC<{ title: string }> = ({ title }) =>
+    (title &&
+        <Text as="span">
+            {title}
+            &nbsp; {/* effectively, this serves as 'min height' */}
+        </Text>
+    ) || (
+        <Text as="span" opacity={0.5}>
+            {"<no title>"}
+        </Text>
+    );
+
+const PropsPreview: React.FC<{ show: boolean }> = ({ show }) =>
+    <Collapse
+        isOpen={show}
+        marginLeft={4}
+    >
+        <Text
+            as="span"
+            fontSize={14}
+            color="rgba(150, 150, 150, 0.3)"
+        >
+            other props will show here
+        </Text>
+    </Collapse>;
+
+const SaveButton: React.FC<{ show: boolean, onSaveClick: () => void }> = ({ show, onSaveClick }) =>
+    <Button
+        variantColor="green"
+        size="sm"
+        visibility={show ? "visible" : "hidden"}
+        marginLeft="auto"  // to keep it on the right
+        flexShrink={0}  // keep shape when other elements in the flex grow bigger
+        border={0}
+        opacity={0.8}
+        onClick={onSaveClick}
+    >
+        Save
+    </Button>;
